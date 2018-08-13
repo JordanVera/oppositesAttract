aaebef7c16bbe1048751b45f3c7605a8d7ee529d
Jordan Vera <verawebdevelopment@gmail.com>
2018-08-13 01:56:17 -0500

    minimum requirements met I believe

---
+++ app/data/users.js
@@ -0,0 +1,29 @@
+let users = [
+    {
+      name: 'Jordan Vera',
+      photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/GodfreyKneller-IsaacNewton-1689.jpg/220px-GodfreyKneller-IsaacNewton-1689.jpg',
+      scores: ['1', '2', '4', '1']
+    },
+    {
+      name: 'Michael Fediuk',
+      photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Einstein_1921_by_F_Schmutzer_-_restoration.jpg/440px-Einstein_1921_by_F_Schmutzer_-_restoration.jpg',
+      scores: ['3', '5', '2', '1']
+    },
+    {
+      name: 'Jennifer Lawerence',
+      photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Jennifer_Lawrence_SDCC_2015_X-Men.jpg/440px-Jennifer_Lawrence_SDCC_2015_X-Men.jpg',
+      scores: ['1', '2', '5', '1']
+    },
+    {
+      name: 'Beyonce',
+      photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Beyonce_-_The_Formation_World_Tour%2C_at_Wembley_Stadium_in_London%2C_England.jpg/440px-Beyonce_-_The_Formation_World_Tour%2C_at_Wembley_Stadium_in_London%2C_England.jpg',
+      scores: ['5', '5', '2', '5']
+    },
+    {
+      name: 'Rihanna',
+      photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Rihanna_concert_in_Washington_DC_%282%29.jpg/500px-Rihanna_concert_in_Washington_DC_%282%29.jpg',
+      scores: ['1', '1', '1', '1']
+    }
+  ];
+  
+  module.exports = users;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,11 +3,39 @@ const users = require("../data/users.js");
 
 module.exports = function (app) {
 
-  app.get("/api/users", (req, res) => res.json(users));
+  app.get("/api/users", (req, res) => {
+    res.json(users)
+  });
 
   app.post("/api/users", (req, res) => {
-    const newIndividual = req.body;
-	
-    let divergences = []
+    const newUser     = req.body;
+    let divergenceArr = [];
+    let matches;
+
+    users.forEach((user) => {
+      divergenceArr.push(divergence(newUser, user));
+    });
+    
+    const bestMatch = Math.min.apply(null, divergenceArr)
+
+    divergenceArr.forEach((diff, i) => {
+      if (diff === bestMatch) {
+        matches = users[i] 
+      }
+    });
+
+    users.push(newUser);
+    users.push(matches)
+    
+  });
+
+
+  function divergence(x, y) {
+    return x.scores.reduce((accumulator, currentScore, i) => {
+      return accumulator + Math.abs(currentScore - y.scores[i])
+    },0)
   }
+
 }
+ 
+
